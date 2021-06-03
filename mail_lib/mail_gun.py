d97943926ea00f599005a0dbf957f87929f7f6a0
ezekiel.eromosei <ezekiel.eromosei@longbridgetech.com>
2021-06-03 10:28:59 +0100

    commiting mailing lib

---
+++ mail_lib/mail_gun.py
@@ -0,0 +1,25 @@
+import os
+from typing import List
+
+from requests import Response, post
+
+
+class Mailgun:
+    MAILGUN_DOMAIN = os.environ.get("MAILGUN_DOMAIN")
+    MAILGUN_API_KEY = os.environ.get("MAILGUN_API_KEY")
+    FROM_TITLE = "Stores REST API"
+    FROM_EMAIL = os.environ.get("FROM_EMAIL")
+
+    @classmethod
+    def send_confirmation_email(cls, email: List[str], subject: str, text: str, html: str) -> Response:
+        return post(
+            f"https://api.mailgun.net/v3/{cls.MAILGUN_DOMAIN}/messages",
+            auth=("api", cls.MAILGUN_API_KEY),
+            data={
+                "from": f"{cls.FROM_TITLE} <{cls.FROM_EMAIL}>",
+                "to": email,
+                "subject": subject,
+                "text": text,
+                "html": html
+            }
+        )

7811b768c66a6b88509f1716acab3b5369a8c470
ezekiel.eromosei <ezekiel.eromosei@longbridgetech.com>
2021-06-03 10:18:59 +0100

    refactored mailing into a class

--- models/user.py
+++ models/user.py
@@ -1,6 +1,11 @@
 from typing import Dict, Union
 
+from flask import request, url_for
+
 from db import db
+from requests import Response
+
+from mail_lib.mail_gun import Mailgun
 
 # CUSTOM JSON TYPES ->> no longer required cos of marshmallow
 # UserJSON = Dict[str, Union[int, str]]
@@ -12,6 +17,7 @@ class UserModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(80), nullable=False, unique=True)
     activated = db.Column(db.Boolean, default=False)
 
     # todo info: no longer needed as nullable=False
@@ -32,5 +38,21 @@ class UserModel(db.Model):
         return cls.query.filter_by(username=username).first()  # .query ==> select * from users, .filter ==> where
 
     @classmethod
+    def find_by_email(cls, email) -> "UserModel":
+        return cls.query.filter_by(email=email).first()
+
+    @classmethod
     def find_by_id(cls, _id: int) -> "UserModel":
         return cls.query.filter_by(id=_id).first()
+
+    def send_confirmation_email(self) -> Response:
+        # root http://localhost:5000
+        # userconfirm is the name of the route i.e UserConfirm in lowercase
+        # hence we have http://localhost:5000/user_confirm/1
+        link = request.url_root[:-1] + url_for("userconfirm", user_id=self.id)
+
+        subject = "Registration Confirmation"
+        text = f"Please click the link to confirm your registration: {link}"
+        html = f'<html>Please click the link to confirm your registration: <a href="{link}">{link}<a/> </html>'
+        return Mailgun.send_confirmation_email([self.email], subject, text, html)
+
--- resources/user.py
+++ resources/user.py
@@ -1,4 +1,5 @@
 import hmac
+import traceback
 
 from flask import request, make_response, render_template, redirect
 from flask_restful import Resource
@@ -10,6 +11,7 @@ from models.user import UserModel
 from blacklist import BLACKLIST
 
 USER_ALREADY_EXISTS = "A user with that username already exists."
+EMAIL_ALREADY_EXISTS = "A user with that email already exists."
 CREATED_SUCCESSFULLY = "User created successfully."
 USER_NOT_FOUND = "User not found."
 USER_DELETED = "User deleted."
@@ -17,6 +19,9 @@ INVALID_CREDENTIALS = "Invalid credentials!"
 USER_LOGGED_OUT = "User <id={user_id}> successfully logged out."
 NOT_CONFIRMED_ERROR = "You have not confirmed registration, please check your email '{}'."
 USER_CONFIRMED = "User confirmed"
+FAILED_TO_CREATE = "Internal server error. Failed to create user"
+SUCCESS_REGISTER_MESSAGE = "Account created successfully, an email with activation link has been sent to your " \
+                           "email address. Please check"
 
 user_schema = UserSchema()
 
@@ -29,9 +34,17 @@ class UserRegister(Resource):
         if UserModel.find_by_username(user.username):
             return {"message": USER_ALREADY_EXISTS}, 400
 
-        user.save_to_db()
+        if UserModel.find_by_email(user.email):
+            return {"message": EMAIL_ALREADY_EXISTS}, 400
 
-        return {"message": CREATED_SUCCESSFULLY}, 201
+        try:
+
+            user.save_to_db()
+            user.send_confirmation_email()
+            return {"message": SUCCESS_REGISTER_MESSAGE}, 201
+        except:
+            traceback.print_exc()
+            return {"message": FAILED_TO_CREATE}, 500
 
 
 class User(Resource):
@@ -61,7 +74,9 @@ class UserLogin(Resource):
     def post(cls):
 
         user_json = request.get_json()
-        user_data = user_schema.load(user_json)     # errors are caught in app.py as general validation err handler
+        # errors are caught in app.py as general validation err handler
+        # email will be ignored, by using partial
+        user_data = user_schema.load(user_json, partial=("email",))
 
         user = UserModel.find_by_username(user_data.username)
 
